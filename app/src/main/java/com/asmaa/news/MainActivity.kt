3d818e68efe1240d04f62e36c5bf00dd773003c6
AsmaaKhaledGomaa <asmaa.kh.gomaa@gmail.com>
2023-04-26 07:09:33 +0200

    bottomNavigationView

--- app/src/main/java/com/asmaa/news/MainActivity.kt
+++ app/src/main/java/com/asmaa/news/MainActivity.kt
@@ -2,32 +2,39 @@ package com.asmaa.news
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.core.view.isVisible
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.asmaa.news.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-//private lateinit var navController: NavController
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater, null, false)
-        setContentView(binding.root)
-        // navController = findNavController(R.id.navHostFragment)
-        //  binding.bottomNavigationView.setupWithNavController(navController)
+        setContentView(R.layout.activity_main)
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
-        val navController = navHostFragment.navController
 
-//        navController.addOnDestinationChangedListener { controller, destination, arguments ->
-//            when (destination.id) {
-//                R.id.newsFragment->
-//                    binding.bottomNavigationView.isVisible = false
-//                else -> binding.bottomNavigationView.isVisible = true
-//            }
-//        }
+      val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+          navController = navHostFragment.navController
+      val bottomNavigationView=findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+          bottomNavigationView.setupWithNavController(navController)
+
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if(destination.id == R.id.splashFragment) {
+
+                bottomNavigationView.visibility = View.GONE
+            } else {
+
+                bottomNavigationView.visibility = View.VISIBLE
+            }
+        }
     }
 }
--- app/src/main/java/com/asmaa/news/ui/NewsDetailsFragment.kt
+++ app/src/main/java/com/asmaa/news/ui/NewsDetailsFragment.kt
@@ -1,31 +1,20 @@
 package com.asmaa.news.ui
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import com.asmaa.news.R
 
-class SplashFragment : Fragment() {
-
-
+class NewsDetailsFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view =inflater.inflate(R.layout.fragment_splash, container, false)
-
-
-        Handler(Looper.myLooper()!!).postDelayed({
-            findNavController().navigate(R.id.action_splashFragment_to_newsFragment)
-        },1000)
-
-        return view
+        return inflater.inflate(R.layout.fragment_news_details, container, false)
     }
+
 }
